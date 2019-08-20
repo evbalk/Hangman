57e868f68cf5880d32ee8d996f34a5e23b540d23
evbalk <52017968+evbalk@users.noreply.github.com>
2019-08-19 22:20:50 -0500

    Update Hangman.py
    Fixed extra letter space. Created difficulties. Show previous guesses.

--- Hangman.py
+++ Hangman.py
@@ -4,21 +4,41 @@ with open("wordlist.txt") as f:
     lines = f.readlines()
 
 rand_num = random.randrange(len(lines))
-WORD = lines[rand_num]
+WORD = lines[rand_num].rstrip()
 
-num_strikes = int(input("How many strikes would you like?"))
+difficulty = input("Difficulty: 'e': 10 strikes, 'm' 7: strikes, 'h': 5 strikes").lower()
+
+if difficulty == 'e':
+    num_strikes = 10
+elif difficulty == 'm':
+    num_strikes = 7
+elif difficulty == 'h':
+    num_strikes = 10
+else:
+    num_strikes = 7
+
 answer = ''
 display = ''
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+initial = ''
+
 for letter in WORD:
     answer += "_"
+    initial += "_ "
 
 past_guesses = ''
+print(initial)
 
+# print(WORD)
+print(str(len(WORD)) + " letters")
+print(".............")
+print("\n")
 while num_strikes > 0 and answer != WORD:
 
+    if past_guesses:
+        print("Already Guessed: %s" % past_guesses)
+
     guess = input("Guess: ").lower()
-    print(guess)
 
     while guess not in alphabet:
         print("Guess was not a letter")
@@ -32,6 +52,7 @@ while num_strikes > 0 and answer != WORD:
         print(guess + " is wrong")
         num_strikes -= 1
     else:
+        print(guess + " is right!")
         for location, letter in enumerate(WORD):
             if letter == guess:
                 answer = answer[:location] + letter + answer[
@@ -41,8 +62,8 @@ while num_strikes > 0 and answer != WORD:
     for ch in answer:
         display += ch + ' '
     print(display)
-    print(str(num_strikes) + " stikes left!")
-    print("---------------\n")
+    print(str(num_strikes) + " strikes left!")
+    print(".............\n")
     past_guesses += guess
 
 if answer == WORD:
